5a35ea16de3a02987bb4328acbcdd86567bf8a5d
suresh kandagatla <surikandagatla@gmail.com>
2020-07-21 11:54:16 -0400

    Finished Creating a Monster

--- src/main/java/com/github/perscholas/StoneMonster.java
+++ src/main/java/com/github/perscholas/StoneMonster.java
@@ -3,5 +3,9 @@ package com.github.perscholas;
 /**
  * Created by leon on 6/10/2020.
  */
-public class StoneMonster {
+public class StoneMonster implements Monster{
+    public String attack(){
+        String output = "Rock";
+        return output;
+    }
 }
