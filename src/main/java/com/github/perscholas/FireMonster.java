b68fe67086e03563d60ad878b708b1bf7035546d
monicadeshmukh <renavikar_monica@yahoo.com>
2020-07-21 10:31:08 -0400

    completed exercise

--- src/main/java/com/github/perscholas/FireMonster.java
+++ src/main/java/com/github/perscholas/FireMonster.java
@@ -1,4 +1,8 @@
 package com.github.perscholas;
 
-public class FireMonster {
+public class FireMonster implements Monster{
+    @Override
+    public String attack() {
+        return "Fire Ball";
+    }
 }
