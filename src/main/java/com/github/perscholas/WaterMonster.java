cb3019b032b0a20340830a99982e5b3714daeebc
AnjaliG927 <anjali.ghaligi@gmail.com>
2020-07-21 10:56:20 -0400

    completed

--- src/main/java/com/github/perscholas/WaterMonster.java
+++ src/main/java/com/github/perscholas/WaterMonster.java
@@ -3,5 +3,11 @@ package com.github.perscholas;
 /**
  * Created by leon on 6/10/2020.
  */
-public class WaterMonster {
+public class WaterMonster implements Monster {
+    String attackMode = "Water Ball";
+
+    @Override
+    public String attack() {
+        return attackMode;
+    }
 }
